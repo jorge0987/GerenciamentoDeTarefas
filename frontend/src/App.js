83e23979d187b348940c073e42e649dae49aad15
Larissa Milena <larissamillena57@gmail.com>
2022-06-07 23:36:59 -0300

    Front-end dos cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  //função para adicionar o que foi digitado ao array de tasks (tasks são cada card: titulo e descrição)
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const id = Date.now();
+
+    const task = {
+      id,
+      title,
+      description
+    }
+
+    setTasks(prev => [...prev, task]);
+
+    setTitle('');
+    setDescription('');
+  }
+
+  const handleDeleteTask = (id) => {
+    //função que vai excluir a task que foi clicada
+    //para excluir, a função vai retornar todas as tasks do array, menos a que foi clicada
+    const newTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(newTasks);
+  }
+
+  //formulário (título e descrição)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <form onSubmit={handleSubmitForm} class="new-task-container">
+        <div className='content-inputs'>
+          <input
+            type="text"
+            class="new-task-input"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Digite o título da tarefa..."
+            required
+          />
+          <textarea
+            className="new-task-input"
+            placeholder="Digite a descrição da tarefa..."
+            onChange={e => setDescription(e.target.value)}
+            value={description}
+            required
+          />
+        </div>
+
+        <button type='submit' class="new-task-button">Adicionar</button>
+      </form>
+
+      {/* área que contém a listagem das tasks. Caso não tenha nenhuma, essa área desaparece*/}
+      {tasks.length > 0 && (
+        <div class="tasks-container">
+
+          {/*array de tasks*/}
+          {tasks.map(task => (
+            <div key={task.title} className='task-item'>
+              <div>
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+              </div>
+
+              {/* botão para excluir a task*/}
+              <button onClick={() => handleDeleteTask(task.id)} className='button-remove'>
+                <i className='far fa-trash-alt'></i>
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
